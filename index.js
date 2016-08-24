163957e9e6dbf65a07796620d7883af5500a2aca
snowdrogon <a787031584@163.com>
2016-08-24 23:37:09 +0800

    Update

--- index.js
+++ index.js
@@ -251,9 +251,7 @@ function loop() {
 
     // limit the rate at which fireworks get launched when mouse is down
     if( limiterTick >= limiterTotal ) {
-        if( mousedown ||  obj.onClick(function(){
-            return true;
-        }) ) {
+        if( mousedown ) {
             // start the firework at the bottom middle of the screen, then set the current mouse coordinates as the target
             fireworks.push( new Firework( cw / 2, ch, mx, my ) );
             limiterTick = 0;
